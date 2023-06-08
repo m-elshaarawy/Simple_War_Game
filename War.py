8b37d3affd48fe11d2f4238a8f349d3d2f3199be
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-06-08 22:46:35 +0300

    complete- Card class

--- War.py
+++ War.py
@@ -5,7 +5,12 @@ SUITES=['Hearts', 'Diamonds', 'Clubs', 'Spades']
 RANKS=['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
 class card:
-    pass
+    def __init__(self, rank, suite):
+        self.rank = rank
+        self.suite = suite
+        
+    def __str__(self):
+        return f'{self.rank} of {self.suite}'
 
 class Deck:
     pass
