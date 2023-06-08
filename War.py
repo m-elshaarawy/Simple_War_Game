27f9e4b89839b7a2b4f415f20947030233e4fb15
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-06-08 22:43:27 +0300

    creat main structure

---
+++ War.py
@@ -0,0 +1,22 @@
+
+from random import shuffle
+
+SUITES=['Hearts', 'Diamonds', 'Clubs', 'Spades']
+RANKS=['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+class card:
+    pass
+
+class Deck:
+    pass
+
+class Hand:
+    pass
+
+class Player:
+    pass
+
+
+class game:
+    pass
+
